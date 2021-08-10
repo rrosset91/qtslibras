c912de03e6c14749162a84bb3133816770e45ff6
ROGER ROSSET <rrosset91@gmail.com>
2021-08-09 23:38:56 -0300

    Dependent picklists working

--- App.js
+++ App.js
@@ -4,29 +4,33 @@ import {StyleSheet, Text, View} from 'react-native'
 import {Picker} from '@react-native-picker/picker'
 import {useState} from 'react'
 import {useFonts, Jost_400Regular} from '@expo-google-fonts/jost'
-const CARS_BRANDS = require('./assets/CarBrands.json')
-const CARS_MODELS = require('./assets/CarModels.json')
+const CARS_BRANDS = require('./assets/Marcas.json')
+const CARS_MODELS = require('./assets/Modelos.json')
+const CARS_VERSIONS = require('./assets/Versoes.json')
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     Jost_400Regular,
   })
+  let resetOptions = () => {
+    setShowVersions(false)
+    setShowModels(false)
+    setFilteredModels([])
+    setAvailableVersions([])
+    setSelectedVersion('')
+  }
   const brands = JSON.parse(JSON.stringify(CARS_BRANDS))
   const models = JSON.parse(JSON.stringify(CARS_MODELS))
-  const [selectedBrand, setSelectedBrand] = useState('Selecione uma marca')
-  const [selectedModel, setSelectedModel] = useState('Selecione um modelo')
-  let [filteredModels, setFilteredModels] = useState()
-  let filterModels = brand => {
-    return models.filter(model => {
-      return model.Marca == brand
-    })
-  }
+  const versions = JSON.parse(JSON.stringify(CARS_VERSIONS))
 
-  let cleanFilteredModels = () => {
-    filteredModels.filter(function (item, pos) {
-      return a.indexOf(item) == pos
-    })
-  }
+  const [selectedBrand, setSelectedBrand] = useState()
+  const [selectedModel, setSelectedModel] = useState()
+  let [selectedVersion, setSelectedVersion] = useState('')
+  let [filteredModels, setFilteredModels] = useState()
+  let [versionsOptions, setAvailableVersions] = useState()
+  let [showVersions, setShowVersions] = useState()
+  let [showModels, setShowModels] = useState()
+
   if (!fontsLoaded) {
     return <AppLoading />
   } else {
@@ -39,10 +43,28 @@ export default function App() {
             itemStyle={styles.pickerItem}
             onValueChange={(itemValue, itemIndex) => {
               setSelectedBrand(itemValue)
-              let filteredMoldels = filterModels(itemValue)
-              setFilteredModels(filteredMoldels)
+              resetOptions()
+              if (
+                itemValue === undefined ||
+                itemValue === 'Selecione uma marca...'
+              ) {
+                setShowModels(false)
+              } else {
+                let cleanedModels = []
+                models[itemValue].forEach(model => {
+                  if (!cleanedModels.includes(model)) cleanedModels.push(model)
+                  setFilteredModels(cleanedModels)
+                })
+                setShowModels(true)
+              }
             }}
           >
+            <Picker.Item
+              label={'Selecione uma marca...'}
+              key={'default'}
+              value={undefined}
+              style={styles.pickerItem}
+            />
             {brands.map((brand, index) => {
               return (
                 <Picker.Item
@@ -54,20 +76,50 @@ export default function App() {
               )
             })}
           </Picker>
-          {!selectedBrand.includes('Selecione uma marca') ? (
+          {showModels == true ? (
             <Picker
               selectedValue={selectedModel}
               style={styles.picker}
               onValueChange={(itemValue, itemIndex) => {
                 setSelectedModel(itemValue)
+                if (itemValue != undefined) {
+                  setShowVersions(true)
+                  let availableVersions = versions.filter(model => {
+                    return model.modelo == itemValue
+                  })
+                  setAvailableVersions(availableVersions)
+                } else {
+                  setShowVersions(false)
+                }
               }}
             >
+              <Picker.Item
+                label="Selecione um modelo..."
+                key={'default'}
+                value={undefined}
+              />
               {filteredModels.map((model, index) => {
+                return <Picker.Item label={model} key={index} value={model} />
+              })}
+            </Picker>
+          ) : null}
+          {showVersions ? (
+            <Picker
+              selectedValue={selectedVersion}
+              style={styles.picker}
+              onValueChange={(itemVersion, itemIndex) => {
+                setSelectedVersion(itemVersion)
+                console.log(itemVersion)
+                console.log(itemIndex)
+              }}
+            >
+              <Picker.Item label={'Escolha uma versão...'} value={undefined} />
+              {versionsOptions.map((version, index) => {
                 return (
                   <Picker.Item
-                    label={model.Modelo}
+                    label={version.versao}
                     key={index}
-                    value={model.Modelo}
+                    value={version.versao}
                   />
                 )
               })}
@@ -99,10 +151,10 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     borderWidth: 2,
-    borderColor: 'red',
+    borderColor: '#708090',
     fontSize: 20,
     fontFamily: 'Jost_400Regular',
-    color: 'red',
+    color: '#708090',
   },
   pickerItem: {
     fontSize: 30,
